eb70bb53ff2fb2119dfb5bcca6c1096192a2362f
brianc <brianc@localhost>
2015-05-04 22:22:02 -0400

    Add constructors, data fields, accessor, mutator, toString

--- DemoProject/src/edu/saintjoe/cs/brianc/demoproject/DemoProject.java
+++ DemoProject/src/edu/saintjoe/cs/brianc/demoproject/DemoProject.java
@@ -4,4 +4,35 @@ package edu.saintjoe.cs.brianc.demoproject;
 
 public class DemoProject {
 
-}
+	// Class to keep track of my pets
+	private String name;
+	private int age;
+	private int petClass; // 1 = dog 2= cat 3 = parakeet, etc.
+	
+	// Constructors
+	public DemoProject(String petName, int petAge, int thisPetClass) {
+		name = petName;
+		age = petAge;
+		petClass = thisPetClass;
+	}
+	
+	public String toString() {
+		return "Pet class: " + petClass + " is named " + name + "Age: " + age;
+	}
+	
+	// Accessors
+	public String getName() {
+		return name;
+	}
+	
+	// likewise for the other two
+	
+	// Mutators
+	public void setName(String newName) {
+		name = newName;
+	}
+	
+	
+	}
+	
+
