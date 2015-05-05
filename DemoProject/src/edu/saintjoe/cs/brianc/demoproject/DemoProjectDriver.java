5c08c00f5d0402bff6b833fdcbe545c89a7de51b
brianc <brianc@localhost>
2015-05-04 22:24:28 -0400

    Add my new objects

--- DemoProject/src/edu/saintjoe/cs/brianc/demoproject/DemoProjectDriver.java
+++ DemoProject/src/edu/saintjoe/cs/brianc/demoproject/DemoProjectDriver.java
@@ -4,6 +4,13 @@ public class DemoProjectDriver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		DemoProject petOne, petTwo, petThree;
+		
+		petOne = new DemoProject("Puffy", 15, 2);
+		petTwo = new DemoProject("Sissy", 2, 1);
+		petThree = new DemoProject("C", 8, 1);
+		
 
 	}
 
